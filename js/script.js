3cc31f6827e9a199905613a42a9c9cfa942a88cc
Jefferson Santana <jefferson.santana1.js@gmail.com>
2023-08-06 12:21:44 -0300

    Inclusão do READNE

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const hoursHand = document.querySelector(".hour-hand")
+const minutesHand = document.querySelector(".minute-hand")
+const secondHand = document.querySelector(".second-hand")
+
+
+
+const getTime = () => {
+    const date = new Date();
+    
+    return  {
+        hours: date.getHours(),
+        minutes: date.getMinutes(),
+        seconds: date.getSeconds(),
+    };
+
+    
+}
+
+setInterval(() => {
+
+        
+    const { seconds, minutes, hours } = getTime();
+    
+    secondHand.style.transform = `translate(0, -50%)rotate(${seconds * 6}deg)`;
+    minutesHand.style.transform = `translate(0, -50%)rotate(${minutes * 6}deg)`;
+    hoursHand.style.transform = `translate(0, -50%)rotate(${hours * 30}deg)`;
+
+}, 1000 );
+
+
